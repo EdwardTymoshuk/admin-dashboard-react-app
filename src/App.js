446f65ff42efb65e79779f3970c3de5fbeec4f58
Eduard Tymoshuk <edward_t@mbp-edward.home>
2022-08-21 22:45:14 +0200

    add Context API

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components'
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages'
 
+import { useStateContext } from './contexts/ContextProvider'
+
 import './App.css'
 
 const App = () => {
-    const activeMenu = true
+    const { activeMenu } = useStateContext()
 
     return (
         <div>
@@ -30,14 +32,14 @@ const App = () => {
                         </TooltipComponent>
                     </div>
                     {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-              <Sidebar />
-            </div>
-          ) : (
-            <div className="w-0 dark:bg-secondary-dark-bg">
-              <Sidebar />
-            </div>
-          )}
+                        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+                            <Sidebar />
+                        </div>
+                    ) : (
+                            <div className="w-0 dark:bg-secondary-dark-bg">
+                                <Sidebar />
+                            </div>
+                        )}
                     <div className={
                         `dark:bg-main-bg
     bg-main-bg min-h-screen w-full ${activeMenu ?
