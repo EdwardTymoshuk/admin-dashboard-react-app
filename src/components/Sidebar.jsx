488afd90254fcb391bfdf7cbd8382ed4ef7ad182
Eduard Tymoshuk <edward_t@mbp-edward.home>
2022-08-22 12:51:57 +0200

    create and configure navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,11 @@ import { useStateContext } from '../contexts/ContextProvider'
 import {links} from '../data/dummy'
 
 const Sidebar = () => {
-    const {activeMenu, setActiveMenu} = useStateContext()
+    const {activeMenu, setActiveMenu, screenSize} = useStateContext()
+
+    const handleCloseSideBar = () => {
+        activeMenu && screenSize <= 900 && setActiveMenu(false)
+    }
 
     const activeLink = 'flex item-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2' 
     const normalLink = 'flex item-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2' 
@@ -20,7 +24,7 @@ const Sidebar = () => {
             {activeMenu && (<>
             <div className="flex justify-between
             items-center">
-                <Link to="/" onClick={() => {setActiveMenu(false)}}
+                <Link to="/" onClick={handleCloseSideBar}
                 className="items-center gap-3 ml-3
                 mt-4 flex text-xl font-extrabold
                 tracking-tight dark:text-white
@@ -45,7 +49,7 @@ const Sidebar = () => {
                         {item.links.map(link => {
                             <NavLink to={`/${link.name}`} 
                             key={link.name} 
-                            onClick={() => {}}
+                            onClick={handleCloseSideBar}
                             className={({isActive}) => isActive ? activeLink : normalLink}
                             >
                                 {link.icon}
